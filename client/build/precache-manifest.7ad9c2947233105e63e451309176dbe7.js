4f0a87edf9d58c9a7ff613b862f2f9a82efec8e8
Webdev-DavidM <footballblubber@googlemail.com>
2020-09-17 09:39:26 +0100

    build completed and tested

---
+++ client/build/precache-manifest.7ad9c2947233105e63e451309176dbe7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd3797765cfd4fdb6edae4cb2d6743cb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bcf43273b2a97e572107",
+    "url": "/static/css/main.28ec31e2.chunk.css"
+  },
+  {
+    "revision": "1716bdd14e70d4a1b817",
+    "url": "/static/js/2.375e94dd.chunk.js"
+  },
+  {
+    "revision": "d4773cfc8952e56a2df4dd3d885d1f23",
+    "url": "/static/js/2.375e94dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bcf43273b2a97e572107",
+    "url": "/static/js/main.c0106cf3.chunk.js"
+  },
+  {
+    "revision": "6f0234b4b87d585d06e8",
+    "url": "/static/js/runtime-main.cfae3202.js"
+  },
+  {
+    "revision": "4b31330b20e3cff3e49d540e4e7175e2",
+    "url": "/static/media/showcase.4b31330b.jpg"
+  }
+]);
